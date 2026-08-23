611de7bafdd756ff08e30997bcbf41b1ba64feed
Yaroslav <112239307+Smet-k@users.noreply.github.com>
2026-08-23 16:17:20 +0300

    lcd: small fixes

--- lcd_i2c/ports/stm32/lcd_i2c_port_stm32.h
+++ lcd_i2c/ports/stm32/lcd_i2c_port_stm32.h
@@ -1,10 +1,10 @@
 #ifndef LCD_I2C_PORT_STM32_H
 #define LCD_I2C_PORT_STM32_H
-#include "stm32f4xx_hal_i2c.h"
 
-static lcd_status_t stm32_i2c_write(uint8_t addr, const uint8_t *data, uint16_t len, void *ctx);
-static void stm32_delay_us(uint32_t us);
-static void stm32_delay_ms(uint32_t ms);
+#include "stm32f4xx_hal.h"
+#include "lcd_i2c/lcd_i2c_status.h"
+#include "lcd_i2c/lcd_i2c_port.h"
+
 void lcd_port_stm32_init(lcd_io_t *io, I2C_HandleTypeDef *hi2c);
 
 #endif
